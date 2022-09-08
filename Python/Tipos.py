fe51cae6f7aecf6abbb1601f9932a921daf037bb
Victor Andrade <101683741+victorandradee@users.noreply.github.com>
2022-09-07 23:07:20 -0300

    Add files via upload

---
+++ Python/Tipos.py
@@ -0,0 +1,5 @@
+v1 = input("Digite algo: ")
+print ("O tipo primitivo desse valor é", type(v1))
+print ("Só tem espaços? ", v1.isspace())
+print ("É um número? ", v1.isnumeric())
+print ("É alfabético? ", v1.isalpha())
